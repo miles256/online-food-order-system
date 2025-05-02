03ee7778daafd4ecb53887e8652fde69c6e171b3
miles256 <mathiasarishabamukama1@gmail.com>
2025-05-03 00:41:57 +0300

    Add files via upload

---
+++ onlinefood-order/admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center"><?php echo date("Y"); ?> . Developed By ARISHABAMUKAMA MATHIAS</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
